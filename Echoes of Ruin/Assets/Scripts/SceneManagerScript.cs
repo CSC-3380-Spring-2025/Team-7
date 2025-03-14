77daf553dd9db3f309a5d32e18817e6c64cfe67c
Amy Tran <amytunhi@gmail.com>
2025-03-14 15:05:28 -0500

    Gacha and Gacha Screen
    Initialized Gacha and Gacha Screen.

--- Echoes of Ruin/Assets/Scripts/SceneManagerScript.cs
+++ Echoes of Ruin/Assets/Scripts/SceneManagerScript.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagerScript : MonoBehaviour {
     // References
-    public void LoadScene(string sceneName){
+    public void LoadScene(string sceneName) {
         SceneManager.LoadScene(sceneName);
     }
 }
