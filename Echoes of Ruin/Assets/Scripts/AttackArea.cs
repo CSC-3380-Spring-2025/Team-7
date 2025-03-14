cf4bd7e7cd908083804c44c3ab7d80d09c99bb12
vrenee0 <renee.vo1000@gmail.com>
2025-03-14 15:44:00 -0500

    health and attack fixes

--- Echoes of Ruin/Assets/Scripts/AttackArea.cs
+++ Echoes of Ruin/Assets/Scripts/AttackArea.cs
@@ -5,14 +5,20 @@ using System.Collections.Generic;
 //If collider has a health then they will recieve damage upon attack
 public class AttackArea : MonoBehaviour 
 {
-   private int damage = 3;
+   private int damage;
 
    private void OnTriggerEnter2D(Collider2D collider)
    {
         if(collider.GetComponent<Health>() != null)
         {
             Health health = collider.GetComponent<Health>();
-            health.Damage(damage) 
+            health.Damage(damage) ;
         }  
    }
+
+//allows attack value to be dynamic
+   public void SetDamage(int newDamage){
+    
+    damage = newDamage;
+   } 
 }
---
+++ Echoes of Ruin/Assets/Scripts/Health.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int health = 100;
+
+    private int MAX_HEALTH = 100;
+
+    //Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    public void Damage(int amount) {
+
+        if (amount < 0){
+            throw new System.ArgumentOutOfRangeException("Cannot have negative health");           
+        }
+
+        this.health -= amount;
+
+        if(health <= 0){
+
+            Die();
+        } 
+    }
+
+    public void Heal(int amount) {
+
+         if (amount < 0){
+            throw new System.ArgumentOutOfRangeException("Cannot have negative healing");           
+        }
+
+        if(health + amount > MAX_HEALTH) {
+            this.health = MAX_HEALTH;
+        }
+        else {
+
+            this.health += amount;
+
+        }
+        
+    }
+
+    private void Die() {
+        Debug.Log("I am Dead!");
+        Destroy(gameObject);
+    }
+}
