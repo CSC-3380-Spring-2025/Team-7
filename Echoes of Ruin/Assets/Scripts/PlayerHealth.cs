e2358d0241b343d8f14ff14d2c95b10502681013
agill32 <ambergill2004@gmail.com>
2025-03-14 16:03:08 -0500

    Keeping track of player hearts and game over

---
+++ Echoes of Ruin/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{   public int playerHP;
+    public GameObject[] hearts; //placeholder?
+
+    private void Start()
+    {   UpdateHP();
+    }
+
+    public void UpdateHP()
+    {   if (playerHP <= 0)
+        {   SceneManager.LoadScene("GameOver"); }
+        else
+        { for (int i = 0; i < hearts.Length; i++)
+            { if (i < playerHP)
+                { hearts[i] = Color.red; }
+              else
+                { hearts[i] = Color.black; }
+            }
+        }
+    }
+}
