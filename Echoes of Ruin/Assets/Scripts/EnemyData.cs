a39656ec59bb9cfc86b02f9b5420868b541dc860
vrenee0 <renee.vo1000@gmail.com>
2025-03-20 21:58:59 -0500

    creating melee and basic attack script for enemies

--- Echoes of Ruin/Assets/Scripts/EnemyData.cs
+++ Echoes of Ruin/Assets/Scripts/EnemyData.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-[CreateAssetMenu(fileName = "NewScriptableObjectScript", menuName = "Scriptable Objects/NewScriptableObjectScript")]
+[CreateAssetMenu(fileName = "data", menuName = "ScriptableObjects/Enemy", order = 1)]
 public class EnemyData : ScriptableObject
 {
     //Script gives you the ability to set enemy hp,damage, and speed in unity 
