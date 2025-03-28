1b4e8f8f2954e4e28146839e80a05d180aabc1d6
agill32 <ambergill2004@gmail.com>
2025-03-28 16:00:24 -0500

    Nurture UI, drag items, part of bar logic

---
+++ Echoes of Ruin/Assets/Scripts/DragItem.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DragItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{   
+    Transform afterDrag;
+    Vector3 temp;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {   temp = transform.position;  //sets current position so it can snap back later
+        afterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+    
+    }
+    public void OnDrag(PointerEventData eventData)
+    {   transform.position = Input.mousePosition; }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {  transform.SetParent(afterDrag);
+       transform.position = temp; //snaps back to original position
+    }
+}
