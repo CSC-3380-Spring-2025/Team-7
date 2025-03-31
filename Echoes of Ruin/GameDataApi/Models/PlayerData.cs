e56f26f56ed0620e20d428c67ff01932625b8ab6
stanlsu <stan13@lsu.edu>
2025-03-31 02:35:22 -0500

    updated database1
    updated and fixed.

---
+++ Echoes of Ruin/GameDataApi/Models/PlayerData.cs
@@ -0,0 +1,21 @@
+using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace GameDataApi.Models {
+    [BsonIgnoreExtraElements]
+    public class PlayerData {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public string PlayerId {get; set; } = null!;
+
+        public int Currency { get; set; }
+        public List<string> OwnedSkins { get; set; } = new List<string>();
+        public string CurrentSkin { get; set; } = "Default";
+
+        [BsonElement("lastupdated")]
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime LastUpdated { get; set; }
+    }
+}
