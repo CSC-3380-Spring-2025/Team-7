c0bddd92c9327321224200d735731607e4e9b14c
stanlsu <stan13@lsu.edu>
2025-04-08 18:55:46 -0500

    added audio script
    added and managed the audio scripts

--- Echoes of Ruin/Assets/Scripts/GachaMachine.cs
+++ Echoes of Ruin/Assets/Scripts/GachaMachine.cs
@@ -7,20 +7,18 @@ using TMPro;
 public class GachaMachine : MonoBehaviour
 { 
     // Public sprite references for different skin rarities
-    public Sprite spriteDefault, spriteRare1, spriteRare2, spriteRare3, spriteRare4, spriteRare5, spriteRare6, 
-        spriteSuperRare1, spriteSuperRare2, spriteSuperRare3, spriteSuperRare4, spriteSuperRare5, spriteSuperRare6, 
-        spriteUltraRare1, spriteUltraRare2, spriteUltraRare3;
+    public Sprite spriteDefault, spriteRare1, spriteRare2, spriteSuperRare1, spriteSuperRare2, spriteUltraRare1;
 
 
     // Lists of skin names by rarity
     [SerializeField] 
-    private List<string> rareSkins = new List<string>{"Rare1", "Rare2", "Rare3", "Rare4", "Rare5", "Rare6"};
+    private List<string> rareSkins = new List<string>{"Rare1", "Rare2"};
     
     [SerializeField]
-    private List<string> superRareSkins = new List<string>{"SuperRare1", "SuperRare2", "SuperRare3", "SuperRare4", "SuperRare5", "SuperRare6"};
+    private List<string> superRareSkins = new List<string>{"SuperRare1", "SuperRare2"};
     
     [SerializeField]
-    private List<string> ultraSkins = new List<string>{"UltraRare1", "UltraRare2", "UltraRare3"};
+    private List<string> ultraSkins = new List<string>{"UltraRare1"};
     
     // Set of skins the player owns
     public HashSet<string> mySkins = new HashSet<string>(){"Default"};
@@ -50,19 +48,9 @@ public class GachaMachine : MonoBehaviour
         skinSprites.Add("Default", spriteDefault);
         skinSprites.Add("Rare1", spriteRare1);
         skinSprites.Add("Rare2", spriteRare2);
-        skinSprites.Add("Rare3", spriteRare3);
-        skinSprites.Add("Rare4", spriteRare4);
-        skinSprites.Add("Rare5", spriteRare5);
-        skinSprites.Add("Rare6", spriteRare6);
         skinSprites.Add("SuperRare1", spriteSuperRare1);
         skinSprites.Add("SuperRare2", spriteSuperRare2);
-        skinSprites.Add("SuperRare3", spriteSuperRare3);
-        skinSprites.Add("SuperRare4", spriteSuperRare4);
-        skinSprites.Add("SuperRare5", spriteSuperRare5);
-        skinSprites.Add("SuperRare6", spriteSuperRare6);
         skinSprites.Add("UltraRare1", spriteUltraRare1);
-        skinSprites.Add("UltraRare2", spriteUltraRare2);
-        skinSprites.Add("UltraRare3", spriteUltraRare3);
     } 
 
 
@@ -71,8 +59,8 @@ public class GachaMachine : MonoBehaviour
     {
         // Determine the roll outcome (64 * 128 * 256 = 2097152)
         int roll = UnityEngine.Random.Range(1, 2097153);
-        int rsRoll = UnityEngine.Random.Range(0, 6); // Roll for rare and super rare skins index
-        int uRoll = UnityEngine.Random.Range(0, 3); // Roll for ultra rare skins index
+        int rsRoll = UnityEngine.Random.Range(0, 2); // Roll for rare and super rare skins index
+        int uRoll = UnityEngine.Random.Range(0, 1); // Roll for ultra rare skins index
 
         if (roll <= 32768) // 1 in 64 chance (2097151/64)
         {
