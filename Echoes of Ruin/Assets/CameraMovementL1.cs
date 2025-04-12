ba5d80a2440150408417f1f2e41a18ccb617b687
kashvii-1 <161757537+kashvii-1@users.noreply.github.com>
2025-04-12 14:38:40 -0500

    camera set up + tutorial draft bounds

--- Echoes of Ruin/Assets/CameraMovementL1.cs
+++ Echoes of Ruin/Assets/CameraMovementL1.cs
@@ -1,19 +1,33 @@
+<<<<<<< Updated upstream
 using Unity.VisualScripting;
 using System.Collections.Generic;
 using System.Collections;
+=======
+>>>>>>> Stashed changes
 using UnityEngine;
 
 public class CameraMovementL1 : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+<<<<<<< Updated upstream
     public float speedOfCam = 1f;
     public Transform targetPosition;
     public float heightCam = 1;
+=======
+    public float camSpeed = 1f;
+    public Transform targetPos;
+>>>>>>> Stashed changes
 
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         Vector3 newPosition = new Vector3(targetPosition.position.x, targetPosition.position.y+heightCam, -10f);
         transform.position = Vector3.Slerp(transform.position, newPosition, speedOfCam*Time.deltaTime);
+=======
+        Vector3 newPosition = new Vector3(targetPos.position.x, targetPos.position.y + 1, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPosition, camSpeed);
+        
+>>>>>>> Stashed changes
     }
 }
---
+++ Echoes of Ruin/Assets/Tests/PlayMode/PlayModeTest1.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using JetBrains.Annotations;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+public class PlayModeTest1
+{
+    // A Test behaves as an ordinary method
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    /*
+    private Health healthcheck;
+    public int i;
+
+    [SetUp]
+    public void SetUp()
+    {
+        GameObject dummyPlayer = new GameObject();
+        healthcheck = dummyPlayer.AddComponent<Health>();
+    }
+    
+    [UnityTest]
+    public IEnumerator PlayModeTest1WithEnumeratorPasses()
+    {
+        healthcheck.currentHearts = 3;
+        healthcheck.Damage(20);
+        int result = healthcheck.hearts[i].activeSelf;
+        Assert.AreEqual(2, result);
+        
+        yield return null;
+    }
+    */
+}
