a6c417c5ce85c0bacd75f0e4b8dba259ee7c6303
kashvii-1 <161757537+kashvii-1@users.noreply.github.com>
2025-04-19 11:48:06 -0500

    fixed cam, added new bounds, did scene transition

--- Echoes of Ruin/Assets/CameraMovementL1.cs
+++ Echoes of Ruin/Assets/CameraMovementL1.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class CameraMovementL1 : MonoBehaviour
 {
-    public float camSpeed = 1f;
+    public float camSpeed = 0.2f;
     public Transform targetPos;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPosition = new Vector3(targetPos.position.x, targetPos.position.y + 1, -10f);
+        Vector3 newPosition = new Vector3(targetPos.position.x, targetPos.position.y + 1, -4f);
         transform.position = Vector3.Slerp(transform.position, newPosition, camSpeed);
         
     }
