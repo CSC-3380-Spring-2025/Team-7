244cd18c87bc5d5e45dd5287c2198f9d7f3f93be
stanlsu <163778326+stanlsu@users.noreply.github.com>
2025-04-21 16:44:57 -0500

    Revert "Revert "Merge branch 'Dev' into fixedCameraAndSceneTransition""

--- Echoes of Ruin/Assets/Scripts/PlayerAttack.cs
+++ Echoes of Ruin/Assets/Scripts/PlayerAttack.cs
@@ -15,6 +15,9 @@ public class PlayerAttack : MonoBehaviour {
     private float timer1 = 0f;
     private float timer2 = 0f;
 
+//Animator
+    public Animator animator;
+
 //variables for range (hairball) attack
     public Transform Aimer;
     public float fireSpeed = 10f;
@@ -22,7 +25,7 @@ public class PlayerAttack : MonoBehaviour {
     //start is called before the first frame update
     void Start() {
         MeleeAttack = transform.GetChild(0).gameObject;
-        RangeAttack = transform.GetChild(1).gameObject;
+        //RangeAttack = transform.GetChild(1).gameObject;
     }
 
     //update is called once per frame
@@ -30,12 +33,12 @@ public class PlayerAttack : MonoBehaviour {
     void Update() {
         if (Input.GetKeyDown(KeyCode.Q)) {
             Attack1();
-            Audios.Instance.PlaySound("Woosh1");
+            //Audios.Instance.PlaySound("Woosh1");
         }
 
         if (Input.GetKeyDown(KeyCode.E)) {
             Attack2();
-            Audios.Instance.PlaySound("Woosh2");
+            //Audios.Instance.PlaySound("Woosh2");
         }
 
         if (melee)
@@ -68,6 +71,8 @@ public class PlayerAttack : MonoBehaviour {
     void Attack1() {
         melee = true;
         MeleeAttack.SetActive(true);
+        //Trigger Animation Attack
+        animator.SetTrigger("Combat");
     }
 
     void Attack2() {
