fd6dfce92cabc7a47ba0ca7626bc0b7c85fd3618
agill32 <ambergill2004@gmail.com>
2025-04-23 04:17:27 -0500

    Shop fix + couple other things

---
+++ Echoes of Ruin/Assets/Scripts/NurtureItemsText.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NurtureItemsText : MonoBehaviour
+{   /*public int ItemID;
+    public TMP_Text QuantityTXT;
+    public GameObject vend;
+    
+    void Start() {
+       vend = GameObject.Find("ShopPopUp");
+    }
+
+    void Update() {
+       QuantityTXT.text = "x" + vend.GetComponent<Vendor>().items[3, ItemID].ToString();
+    }*/
+}
