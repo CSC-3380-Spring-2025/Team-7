427d1429278c21ae236a47bda7e3413b90b09cc9
agill32 <ambergill2004@gmail.com>
2025-05-01 21:06:08 -0500

    Update PlayerHealth.cs

--- Echoes of Ruin/Assets/Scripts/PlayerHealth.cs
+++ Echoes of Ruin/Assets/Scripts/PlayerHealth.cs
@@ -11,15 +11,17 @@ public class PlayerHealth : MonoBehaviour
     private void Start()
     {   UpdateHP(); }
 
-    public void UpdateHP()
-    {   scene = SceneManager.GetActiveScene();
+    public void Update() 
+    { scene = SceneManager.GetActiveScene();
         sceneName = scene.name;
         if (sceneName == "TutorialScene")
         { healthUI.SetActive(true);}
         else 
         { healthUI.SetActive(false);}
-      
-      if (playerHP <= 0){
+      }
+
+    public void UpdateHP()
+    {  if (playerHP <= 0){
           SceneManager.LoadScene("GameOver");
           Audios.Instance.PlayMusic("GameOver"); 
           }else{
