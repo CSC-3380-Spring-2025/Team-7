490f0b57fcf5e8812945320dab2268de94526663
agill32 <ambergill2004@gmail.com>
2025-05-03 03:43:20 -0500

    part 2-- drag works now!!

--- Echoes of Ruin/Assets/Scripts/DragItem.cs
+++ Echoes of Ruin/Assets/Scripts/DragItem.cs
@@ -3,11 +3,18 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class DragItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
-{   
+{   public int itemID;
     Transform afterDrag;
     Vector3 temp;
     Vector3 mousePos;
+    ItemTrack items;
+    NurtureSlider sliders;
 
+    void Start() 
+    {   items = GameObject.Find("HeartsAndCoinsOverlay").GetComponent<ItemTrack>();
+        sliders = GameObject.Find("SliderCanvas").GetComponent<NurtureSlider>();
+    }
+    
     public void OnBeginDrag(PointerEventData eventData)
     {   temp = transform.position;  //sets current position so it can snap back later
         afterDrag = transform.parent;
@@ -22,8 +29,26 @@ public class DragItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
     {   mousePos = Input.mousePosition;
         transform.SetParent(afterDrag);
         if (mousePos.x > 354 && mousePos.x < 700 && mousePos.y > 227 && mousePos.y < 600)
-        {   
-           
+        {   switch(itemID)
+            {   case(1):
+                    if (items.ball > 0)
+                    { items.ball--;
+                      sliders.playAmount = sliders.playAmount + 10; 
+                    }
+                    break;
+                case(2):
+                    if (items.bisc > 0)
+                    { items.bisc--; 
+                      sliders.hungerAmount = sliders.hungerAmount + 10; 
+                    }
+                    break;
+                case(3):
+                    if (items.brush > 0)
+                    { items.brush--; 
+                      sliders.cleanAmount = sliders.cleanAmount + 10;
+                    }
+                    break;
+            }
         }
         transform.position = temp; //snaps back to original position
     }
