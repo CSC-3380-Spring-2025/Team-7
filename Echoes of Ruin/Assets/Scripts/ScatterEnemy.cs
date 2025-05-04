40a5f47470248adbcdb7b08e96b1e6fc9101b0fe
JJoestar4 <jnever3@lsu.edu>
2025-05-04 16:45:20 -0500

    Fixed Scatter Boi
    He is now fixed

--- Echoes of Ruin/Assets/Scripts/ScatterEnemy.cs
+++ Echoes of Ruin/Assets/Scripts/ScatterEnemy.cs
@@ -17,8 +17,8 @@ public class ScatterEnemy : MonoBehaviour, IDamageable
     [SerializeField]
     private EnemyData data;
     [SerializeField]
-    // private int damage = 1;
-    // [SerializeField]
+    private int damage = 1;
+    [SerializeField]
       private float speed = 1.5f;  
     [SerializeField] private float stopDistance = 2f; // How close the enemy is allowed to get
 
@@ -28,7 +28,6 @@ public class ScatterEnemy : MonoBehaviour, IDamageable
         shootCooldown = startShootCooldown;
         PlayerCat = GameObject.FindGameObjectWithTag("PlayerCat");
         SetEnemyValues();
-        //currentHP = data.hp;
     }
 
     //checks if cooldown is at 0 and shoots bullets if yes, if no then timer decreases  
@@ -44,10 +43,6 @@ public class ScatterEnemy : MonoBehaviour, IDamageable
        } else {
         shootCooldown -= Time.deltaTime;
        }
-       if (Input.GetKeyDown(KeyCode.K))
-        {
-            TakeDamage(999);
-        }
     }
 
     private void Swarm()
@@ -66,9 +61,9 @@ public class ScatterEnemy : MonoBehaviour, IDamageable
  private void SetEnemyValues()
     {
         currentHP = data.hp;
-        // GetComponent<Health>().SetHearts(currentHP, currentHP);
-        // damage = data.damage;
-        // speed = data.speed;
+        GetComponent<Health>().SetHearts(currentHP, currentHP);
+        damage = data.damage;
+        speed = data.speed;
 
     }
 
@@ -77,19 +72,30 @@ public class ScatterEnemy : MonoBehaviour, IDamageable
         float angleStep = spreadAngle / (bulletCount - 1); // Angle between bullets 
         float startAngle = -spreadAngle / 2; //Starting angle
 
+        Vector2 shootDirection = (PlayerCat.transform.position - transform.position).normalized;
+        float baseAngle = Mathf.Atan2(shootDirection.y, shootDirection.x) * Mathf.Rad2Deg;
+
         for (int i = 0; i < bulletCount; i++)
         {
-            float bulletAngle = startAngle + (angleStep * i);
-            Quaternion rotation = Quaternion.Euler(0, 0, transform.eulerAngles.z + bulletAngle);
+            float currentAngle = baseAngle + startAngle + (i * angleStep);
+            float rad = currentAngle * Mathf.Deg2Rad;
 
-             Instantiate(Scatter, transform.position, transform.rotation); // spawns bulltets at enemy location 
+            Vector2 direction = new Vector2(Mathf.Cos(rad), Mathf.Sin(rad));
+
+            GameObject bullet = Instantiate(Scatter, transform.position, Quaternion.Euler(0, 0, currentAngle)); // spawns bulltets at enemy location
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+
+            if (rb != null)
+            {
+                rb.linearVelocity = direction * 10f; // Use your desired bullet speed
+            }
         }
     }
 
-    public void TakeDamage(int dmg)
+    public void TakeDamage(int damage)
     {
-        currentHP -= dmg;
-        Debug.Log($"{gameObject.name} took {dmg} damage. HP: {currentHP}");
+        currentHP -= damage;
+        Debug.Log($"{gameObject.name} took {damage} damage. HP: {currentHP}");
 
         if (currentHP <= 0)
         {
